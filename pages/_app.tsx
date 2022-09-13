f5705ae32f694c4b5fe3671972aba786fdab80e3
Ruben <donowenrt@gmail.com>
2022-09-13 08:13:31 +0200

    fixed fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "../theme/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme/theme";
-import Fonts from "../theme/fonts";
+import Fonts from "../theme/Fonts";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
