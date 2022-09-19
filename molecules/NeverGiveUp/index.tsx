2bb8d8fe30aa2368c1b6c38b72a051817f44e7a7
Ruben <donowenrt@gmail.com>
2022-09-19 12:34:07 +0200

    fixed mobile changed to svg

--- molecules/NeverGiveUp/index.tsx
+++ molecules/NeverGiveUp/index.tsx
@@ -1,11 +1,11 @@
 import { Box } from "@chakra-ui/react";
 import Image from "next/image";
 import React from "react";
-import NeverGiveUpBanner from "/public/img/NeverGiveUpBanner.png";
+import NeverGiveUpBanner from "/public/img/NeverGiveUpBanner.svg";
 
 const NeverGiveUp = () => {
   return (
-    <Box position="relative" w="full" h="500px">
+    <Box position="relative" w="full" h={{ base: "150px", md: "500px" }}>
       <Image
         src={NeverGiveUpBanner}
         alt="neverg"
